635ba79e9104a7bc983661a7118505c24d73451d
Carlos Rueda <crueda578@unab.edu.co>
2025-03-15 12:10:18 -0500

    html actualizado

--- ProyectoLaravel/routes/web.php
+++ ProyectoLaravel/routes/web.php
@@ -2,6 +2,51 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/index', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/blog_details', function () {
+    return view('blog_details');
+})->name('blog_details');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/contact_process', function () {
+    return view('contact_process');
+})->name('contact_process');
+
+Route::get('/elements', function () {
+    return view('elements');
+})->name('elements');
+
+Route::get('/help', function () {
+    return view('help');
+})->name('help');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/main', function () {
+    return view('main');
+})->name('main');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
